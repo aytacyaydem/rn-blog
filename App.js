64def367d6e686e348ba4bc4505da567b2d7fa09
Aytac <aytacyaydem@gmail.com>
2020-08-31 00:38:16 +0300

    Context provider oluşturuldu ve App.js dosyasına wrapper olarak eklendi

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from './src/screens/HomeScreen';
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import {BlogProvider} from "./src/context/BlogContext"
 
-const AppNavigator = createStackNavigator({
-  Home:HomeScreen
-},{
-  initialRouteName:"Home",
-  defaultNavigationOptions:{
-    title:"Blogs",
-    headerTitleAlign:"center",
-    headerTitleStyle:{
-      color:"white"
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "Blogs",
+      headerTitleAlign: "center",
+      headerTitleStyle: {
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "purple",
+      },
     },
-    headerStyle:{
-      backgroundColor:"purple"
-    }
   }
-})
+);
 
-export default createAppContainer(AppNavigator)
+const App = createAppContainer(AppNavigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  ) 
+  
+};
