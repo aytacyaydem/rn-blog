d01e95e9671d5846d890fea5d905929a5d878836
Aytac <aytacyaydem@gmail.com>
2020-09-05 17:35:49 +0300

    form component oluşturuldu ve create, edit alanlarında aynı form component yerleştirildi

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,41 +1,16 @@
-import React,{useState,useContext} from "react"
-import {View,StyleSheet,Text,TextInput,Button} from "react-native"
+import React,{useContext} from "react"
+import {StyleSheet} from "react-native"
 import {Context} from "../context/BlogContext"
+import BlogForm from "../components/BlogForm"
 
 const CreateScreen = ({navigation}) => {
-    const [title,setTitle] = useState("")
-    const [content,setContent] = useState("")
     const {addBlogPost} = useContext(Context)
-    return (
-       <View>
-           <Text style={styles.label}>Enter Title</Text>
-           <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)}/>
-           <Text style={styles.label}>Enter Content</Text>
-           <TextInput style={styles.input} value={content} onChangeText={text => setContent(text)}/>
-           <Button onPress={() => {
-               addBlogPost(title,content,() => {
-                navigation.navigate("Home")
-               })
-               }} title="Add New Post" />
-       </View>
-    )
+    return <BlogForm />
 }
 
 
 const styles = StyleSheet.create({
-    input:{
-        fontSize:18,
-        borderWidth:1,
-        borderColor:"black",
-        marginBottom:15,
-        padding:5,
-        margin:5
-    },
-    label:{
-        fontSize:20,
-        marginBottom:5,
-        marginLeft:5
-    }
+   
 })
 
 export default CreateScreen
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,17 +1,14 @@
-import React, { useState, useContext } from "react";
-import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogForm from "../components/BlogForm";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
   const id = navigation.getParam("id");
   const editPost = state.find((post) => post.id === id);
-  return (
-    <View>
-      <Text>Editlenecek Content Title : {editPost.title}</Text>
-      <Text>Editlenecek Content: {editPost.content}</Text>
-    </View>
-  );
+  
+  return <BlogForm />
 };
 
 const styles = StyleSheet.create({});
